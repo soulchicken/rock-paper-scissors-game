4255ec21d0ae7ebc060a5c08aac0969b542269b6
solhee-hwang <solhee0912@kookmin.ac.kr>
2022-05-16 19:53:49 +0900

    FEAT : Calculate rank

--- src/main/java/com/game/App.java
+++ src/main/java/com/game/App.java
@@ -2,13 +2,16 @@
 package com.game;
 
 import com.game.controller.GameController;
+import com.game.controller.MenuController;
 
 
 public class App {
 
 	public static void main(String[] args) {
   	GameController gameController = new GameController();
-		gameController.joinUser();
+//		gameController.joinUser();
+		MenuController menuController = new MenuController();
+		menuController.main();
 	}
 
 }
--- src/main/java/com/game/dao/GameDAO.java
+++ src/main/java/com/game/dao/GameDAO.java
@@ -58,15 +58,24 @@ public class GameDAO {
 	}
 	public List<Game> calculateRank(List<Game> games){
 		for (Game game : games) {
-			calOdds = (float) (game.getWin() * 100 /game.getTotalgames());
+			if(game.getTotalgames() ==0){
+				calOdds =0;
+			}else{
+				calOdds = (float) (game.getWin() * 100 /game.getTotalgames());
+			}
 			game.setOdds(calOdds);
 			game = new Game(game.getUser_id(),game.getOdds());
 			oddsRate.add(game);
 		}
+		Game dumy;
 		calGames.add(oddsRate.get(0));
-		for(int i=0;i<games.size();i++) {
-			for(int j=1;j<games.size();j++) {
-				if(oddsRate.get(i).getOdds() <= oddsRate.get(j).getOdds());
+		for(int i=0;i<games.size()-1;i++) {
+			for(int j=i+1;j<games.size();j++) {
+				if(oddsRate.get(i).getOdds() <= oddsRate.get(j).getOdds()){
+					dumy = oddsRate.get(i);
+					oddsRate.set(i, oddsRate.get(j));
+					oddsRate.set(j,dumy);
+				};
 			}
 		}
 
