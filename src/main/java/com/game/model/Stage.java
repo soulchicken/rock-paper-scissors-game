2fcc6e126580a44397664936bf9d5bec93197df6
소울치킨 <soulfever01@naver.com>
2022-05-23 14:50:20 +0900

    REFACTOR : 게임 종료 시그널 View로 이동

--- src/main/java/com/game/model/Stage.java
+++ src/main/java/com/game/model/Stage.java
@@ -48,8 +48,7 @@ public class Stage {
 			int cn = scanner.nextInt();
 
 			if (cn == GAME_END_NUMBER) {
-				System.out.println("게임을 종료합니다.");
-				System.out.println("최종 스코어 - 이긴 횟수 : " + winScore + ", 진 횟수 : " + loseScore + ", 비긴 횟수 : " + drawScore);
+				stageView.EndOfStage(winScore, loseScore, drawScore);
 				dao.save(id, winScore, loseScore, drawScore);
 				
 				break;
--- src/main/java/com/game/view/StageView.java
+++ src/main/java/com/game/view/StageView.java
@@ -9,4 +9,9 @@ public class StageView {
 		String[] rsWinner = { "WIN!!", "LOSE", "DRAW" };
 		System.out.println(rsWinner[rsInt]);
 	}
+	
+	public void EndOfStage(int winScore, int loseScore, int drawScore) {
+		System.out.println("게임을 종료합니다.");
+		System.out.println("최종 스코어 - 이긴 횟수 : " + winScore + ", 진 횟수 : " + loseScore + ", 비긴 횟수 : " + drawScore);
+	}
 }
